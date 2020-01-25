1db839437b89e2b22ab7f163be1d8f19e00d1827
Aimen-Zelaci <53104539+Aimen-Zelaci@users.noreply.github.com>
2020-01-25 17:03:19 +0200

    Delete auth-0.env.js

--- src/auth-0/config/auth-0.env.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-    domain: 'dev-esypoo9e.eu.auth0.com',
-    clientId: 'X59c6ar1ja7n9iDyrvTVdoYT13kf91sW',
-    //callbackUrl: 'https://localhost:3000/',
-    container: 'auth0-login-container'
-}
